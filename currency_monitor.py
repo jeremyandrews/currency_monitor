abddcfd38d755bdf929ec446cf4e1a006a99d9bb
Jeremy Andrews <jeremy@tag1consulting.com>
2014-07-19 12:38:57 -0700

    Only download data once per hour.

--- currency_monitor.py
+++ currency_monitor.py
@@ -3,6 +3,8 @@ import os
 import urllib
 import json
 import sys
+import datetime
+import time
 import sqlite3
 
 def readConfiguration():
@@ -16,7 +18,7 @@ def readConfiguration():
 	else:
 		# Default currency provided by Open Exhange Rates is USD.
 		config['currency_from'] = 'USD'
-	# @tado: track conversion to multiple currencies
+	# @todo: track conversion to multiple currencies
 	if (parser.has_option('openexchangerates', 'currency_to')):
 		config['currency_to'] = parser.get('openexchangerates', 'currency_to')
 	else:
@@ -52,23 +54,27 @@ def getCurrencyList(config):
 def getCurrencyValue(config):
 	conversionURL = "https://openexchangerates.org/api/latest.json?app_id=" + config['app_id'] + "&base=" + config['currency_from']
 	#print conversionURL
-	#conversion = urllib.urlopen(conversionURL)
-	#conversions = conversion.read()
-	#conversion.close()
-	#value = json.loads(conversions)
+	conversion = urllib.urlopen(conversionURL)
+	conversions = conversion.read()
+	conversion.close()
+	value = json.loads(conversions)
 
-	### temporary data for testing
-	value = dict()
-	value['timestamp'] = 1405789261
-	value['rates'] = dict()
-	value['rates']['EUR'] = 0.739362
+	### TEMPORARY DATA for testing
+	#value = dict()
+	#value['timestamp'] = 1405789261
+	#value['rates'] = dict()
+	#value['rates']['EUR'] = 0.739362
 	return value
 
 def getLastTimestamp(config, db):
-	db['cursor'].execute("""SELECT MAX(timestamp) FROM history WHERE currency_from = :from and currency_to = :to""", {"from": config['currency_from'], "to": config['currency_to']})
+	db['cursor'].execute("""SELECT MAX(timestamp) FROM history WHERE currency_from = :from and currency_to = :to""",
+	  {"from": config['currency_from'], "to": config['currency_to']})
 	db['connection'].commit()
 	row = db['cursor'].fetchone()
-	return row[0]
+	if (row[0]):
+		return row[0]
+	else:
+		return 0
 
 # Parse the response from OpenExchangeRates.org.
 # @todo: improve error handling.
@@ -80,26 +86,35 @@ def parseCurrencyValue(config, db, currencyValue):
 		#             u'BGN': 1.438379, u'PAB': 1, u'BOB': 6.901406, u'DKK': 5.477647, u'BWP': 8.841668,
         #              ...
 		#            u'JPY': 101.6395, u'SDG': 5.69271, u'XAU': 0.00075653},
-		#           u'license': u'Data sourced from various providers with public-facing APIs; copyright may apply; resale is prohibited; no warranties given of any kind. Bitcoin data provided by http://coindesk.com. All usage is subject to your acceptance of the License Agreement available at: https://openexchangerates.org/license/', u'disclaimer': u'Exchange rates are provided for informational purposes only, and do not constitute financial advice of any kind. Although every attempt is made to ensure quality, NO guarantees are given whatsoever of accuracy, validity, availability, or fitness for any purpose - please use at your own risk. All usage is subject to your acceptance of the Terms and Conditions of Service, available at: https://openexchangerates.org/terms/'}
+		#           u'license': u'Data sourced from various providers with public-facing APIs; ...'}
 		#print "1 " + fullNames[currency_from] + " = " + str(value['rates'][currency_to]) + " " + fullNames[currency_to] + "s"
-		db['cursor'].execute("""INSERT INTO history (timestamp, currency_from, currency_to, value) VALUES (?, ?, ?, ?);""", (currencyValue['timestamp'], config['currency_from'], config['currency_to'], currencyValue['rates'][config['currency_to']]))
+		db['cursor'].execute("""INSERT INTO history (timestamp, currency_from, currency_to, value) VALUES (?, ?, ?, ?);""",
+		  (currencyValue['timestamp'], config['currency_from'], config['currency_to'], currencyValue['rates'][config['currency_to']]))
 		db['connection'].commit()
 	elif (currencyValue['error']):
 		# ERROR: sample reply
 		#        {u'status': 403, u'message': u'not_allowed', u'description':
-		#         u'Changing `base` currency is only available for Enterprise and Unlimited clients - please upgrade, or contact support@openexchangerates.org with any questions. Thanks!',
+		#         u'Changing `base` currency is only available for Enterprise and Unlimited clients ...',
 		#         u'error': True}
 		print currencyValue['description']
 		sys.exit(-1)
 
 ### MAIN
 
+# read configuration file
 config = readConfiguration()
+# connect to the database
 db = prepDatabase(config)
+# get list of all currencies
 currencyList = getCurrencyList(config)
+# check when we last updated currency
 lastTimestamp = getLastTimestamp(config, db)
-currencyValue = getCurrencyValue(config)
-parseCurrencyValue(config, db, currencyValue)
-closeDatabase(config, db)
 
-print currencyValue
+# download new currency data every hour.
+if ((lastTimestamp + 60 * 60) < time.time()):
+	currencyValue = getCurrencyValue(config)
+	parseCurrencyValue(config, db, currencyValue)
+else:
+	print "No data downloaded. Next download in {:,} seconds.".format(int((lastTimestamp + 60 * 60) - time.time()))
+
+closeDatabase(config, db)
